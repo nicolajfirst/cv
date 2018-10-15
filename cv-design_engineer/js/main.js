8d2365b08514f054c401273f3add95c4d60f0e8e
Nick <nicolajfirst@gmail.com>
2018-10-15 17:02:43 +0300

    cv

---
+++ cv-design_engineer/js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+    $('.header__slide-wrap').slick();
+
+    $('.js-drawing--1').click(function () {
+        $('.js-overlay--1').fadeIn();
+    });
+
+    $('.js-drawing--2').click(function () {
+        $('.js-overlay--2').fadeIn();
+    });
+
+    $('.js-drawing--3').click(function () {
+        $('.js-overlay--3').fadeIn();
+    });
+
+    $('.js-drawing--4').click(function () {
+        $('.js-overlay--4').fadeIn();
+    });
+
+    $('.js-drawing--5').click(function () {
+        $('.js-overlay--5').fadeIn();
+    });
+
+    $('.js-drawing--6').click(function () {
+        $('.js-overlay--6').fadeIn();
+    });
+
+    $('.js-drawing--7').click(function () {
+        $('.js-overlay--7').fadeIn();
+    });
+
+    $('.js-drawing--8').click(function () {
+        $('.js-overlay--8').fadeIn();
+    });
+
+    $('.js-drawing--9').click(function () {
+        $('.js-overlay--9').fadeIn();
+    });
+
+    $('.js-drawing--10').click(function () {
+        $('.js-overlay--10').fadeIn();
+    });
+
+    $('.js-drawing--11').click(function () {
+        $('.js-overlay--11').fadeIn();
+    });
+
+    $('.js-drawing--12').click(function () {
+        $('.js-overlay--12').fadeIn();
+    });
+
+
+    $('.js-close').click(function () {
+       $('.js-overlay').fadeOut();
+
+    });
+});
